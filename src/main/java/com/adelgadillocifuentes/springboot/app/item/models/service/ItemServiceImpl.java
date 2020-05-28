b3b20c10f1ebeef74d3dbc0d0d56f87965ee88b6
Alvaro Delgadillo Cifuentes <adelgadillocifuentes@gmail.com>
2020-05-28 14:28:46 -0500

    Add AppConfig with RestTemplate

--- src/main/java/com/adelgadillocifuentes/springboot/app/item/models/service/ItemServiceImpl.java
+++ src/main/java/com/adelgadillocifuentes/springboot/app/item/models/service/ItemServiceImpl.java
@@ -2,13 +2,18 @@ package com.adelgadillocifuentes.springboot.app.item.models.service;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.adelgadillocifuentes.springboot.app.item.models.Item;
 
 @Service
 public class ItemServiceImpl implements ItemService {
 
+	@Autowired
+	private RestTemplate clienteRest;
+	
 	@Override
 	public List<Item> findAll() {
 		return null;
